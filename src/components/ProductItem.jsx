5771becec080cd0726d6e79415e87cea69372359
JaRafCaPer <caballeroperezjavier@gmail.com>
2023-12-20 18:12:29 -0500

    Arreglando estilos

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,31 +1,57 @@
-import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, Image } from 'react-native';
+import { colors } from '../global/colors';
 
-export default function ProductItem({product, navigation}) {
+export default function ProductItem({ product, navigation }) {
   return (
-    <TouchableOpacity onPress={() => navigation.navigate("Details", product.id)} style={styles.productItemContainer} >
-            <Text style={styles.productItemTitle}>{product.title}</Text>
-            <Image
-                style={styles.productItemImage}
-                resizeMode="cover"
-                source={{uri: product.thumbnail}}
-            />
+    <TouchableOpacity
+      onPress={() => navigation.navigate("Details", product.id)}
+      style={styles.productItemContainer}
+    >
+      <Image
+        style={styles.productItemImage}
+        resizeMode="cover"
+        source={{ uri: product.thumbnail }}
+      />
+      <View style={styles.productItemDetails}>
+        <Text style={styles.productItemTitle}>{product.title}</Text>
+        <Text style={styles.productItemPrice}>${product.price}</Text>
+      </View>
     </TouchableOpacity>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    productItemContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        padding: 20,
-        margin: 20,
-    },
-    productItemTitle: {
-        fontFamily: 'RobotoSerif_28pt_Condensed-Regular',
-        paddingVertical: 20,
-    },
-    productItemImage: {
-        width: 60,
-    }
-}) 
+  productItemContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    padding: 15,
+    margin: 10,
+    backgroundColor: colors.backgroundLight,
+    borderRadius: 10,
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    elevation: 3,
+  },
+  productItemImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 8,
+  },
+  productItemDetails: {
+    marginLeft: 15,
+  },
+  productItemTitle: {
+    fontFamily: 'RobotoSerif_28pt_Condensed-Regular',
+    fontSize: 16,
+    color: colors.textDark,
+  },
+  productItemPrice: {
+    fontFamily: 'RobotoSerif_28pt_Condensed-Bold',
+    fontSize: 18,
+    color: colors.primary,
+    marginTop: 5,
+  },
+});
--- src/screens/ProductDetailScreen.jsx
+++ src/screens/ProductDetailScreen.jsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
-import products_data from '../data/products_data.json';
+import React, { useEffect, useState } from 'react';
 import { View, ScrollView, Text, StyleSheet, ActivityIndicator, Image, TouchableOpacity } from 'react-native';
+import products_data from '../data/products_data.json';
+import { colors } from '../global/colors';
 
 const ProductDetailScreen = ({ route }) => {
   const [productSelected, setProductSelected] = useState({});
@@ -14,26 +15,29 @@ const ProductDetailScreen = ({ route }) => {
     setIsLoading(false);
   }, [productId]);
 
+  const handleBuyPress = () => {
+
+    console.log('Buy button pressed');
+  };
+
   return (
     <>
       {isLoading ? (
         <ActivityIndicator />
       ) : (
         <ScrollView>
-          <View>
-            <Image
-              source={{ uri: productSelected.images[0] }}
-              resizeMode="cover"
-              style={styles.imageProduct}
-            />
-            <View style={styles.DetailContainer}>
-              <Text style={styles.title}> {productSelected.title}</Text>
-              <Text style={styles.description}> {productSelected.description}</Text>
-              <Text style={styles.price}> ${productSelected.price}</Text>
-              <TouchableOpacity style={styles.buyButton} onPress={() => null}>
-                <Text style={styles.buyButtonText}>Buy</Text>
-              </TouchableOpacity>
-            </View>
+          <Image
+            source={{ uri: productSelected.images[0] }}
+            resizeMode="cover"
+            style={styles.imageProduct}
+          />
+          <View style={styles.detailContainer}>
+            <Text style={styles.title}>{productSelected.title}</Text>
+            <Text style={styles.description}>{productSelected.description}</Text>
+            <Text style={styles.price}>${productSelected.price}</Text>
+            <TouchableOpacity style={styles.buyButton} onPress={handleBuyPress}>
+              <Text style={styles.buyButtonText}>Buy</Text>
+            </TouchableOpacity>
           </View>
         </ScrollView>
       )}
@@ -41,32 +45,42 @@ const ProductDetailScreen = ({ route }) => {
   );
 };
 
-export default ProductDetailScreen;
-
 const styles = StyleSheet.create({
   imageProduct: {
-    width: '100%',
+    width: '80%',
     height: 300,
+    alignSelf: 'center',
+    marginTop: 20,
+    marginBottom: 50,
   },
-  DetailContainer: {
-    padding: 10,
+  detailContainer: {
+    padding: 20,
+    backgroundColor: colors.backgroundLight,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    marginTop: -20,
   },
   title: {
-    fontSize: 20,
+    fontSize: 24,
     fontWeight: 'bold',
+    color: colors.textDark,
+    marginBottom: 10,
   },
   description: {
-    fontSize: 16,
+    fontSize: 18,
+    color: colors.textDark,
+    marginBottom: 15,
   },
   price: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: colors.primary,
+    marginBottom: 15,
   },
   buyButton: {
-    marginTop: 10,
-    backgroundColor: 'black',
-    padding: 10,
-    borderRadius: 5,
+    backgroundColor: colors.primary,
+    padding: 15,
+    borderRadius: 8,
   },
   buyButtonText: {
     color: 'white',
@@ -75,3 +89,5 @@ const styles = StyleSheet.create({
     fontSize: 18,
   },
 });
+
+export default ProductDetailScreen;
